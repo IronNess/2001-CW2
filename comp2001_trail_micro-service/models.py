79e28c54963d400304ff099ab87b5d2fceb997c3
Ness <146290658+IronNess@users.noreply.github.com>
2025-01-11 22:46:15 +0000

    Update models.py
    Code imported from DLE

--- comp2001_trail_micro-service/models.py
+++ comp2001_trail_micro-service/models.py
@@ -1,21 +1,26 @@
+# models.py
 
-from app import db
+from datetime import datetime
+# from zoneinfo import ZoneInfo
+import pytz
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+from config import db, ma
 
-class Trail(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class Person(db.Model):
+    __tablename__ = "person"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lname = db.Column(db.String(32), unique=True)
+    fname = db.Column(db.String(32))
+    timestamp = db.Column(
+        db.DateTime, default=lambda: datetime.now(pytz.timezone('Europe/London')),
+        onupdate=lambda: datetime.now(pytz.timezone('Europe/London'))
+    )
 
-class Location(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    trail_id = db.Column(db.Integer, db.ForeignKey('trail.id'), nullable=False)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    sequence = db.Column(db.Integer, nullable=False)
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+        sql_session = db.session
 
+person_schema = PersonSchema()
+people_schema = PersonSchema(many=True)
