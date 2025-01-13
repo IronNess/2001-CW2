274fafb3cf0a76dcd4d48267902999acbc451ab9
Ness <146290658+IronNess@users.noreply.github.com>
2025-01-13 17:29:13 +0000

    Update app.py
    Comment removed to clean code

--- comp2001_trail_micro-service/app.py
+++ comp2001_trail_micro-service/app.py
@@ -3,7 +3,7 @@ from config import connex_app, db
 if __name__ == "__main__":
     connex_app.add_api(
         "swagger.yml",
-        base_path="/api",  # or remove/change this as needed
+        base_path="/api",  
         options={"swagger_ui": True},
         arguments={"title": "Trail Application REST API"},
     )
