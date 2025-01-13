5b51614f0fa4b824f83dc779015df19846caab1e
Ness <146290658+IronNess@users.noreply.github.com>
2025-01-13 19:27:04 +0000

    Update models.py
    class tag and trail tag added to models. Stated keys under db schema

--- comp2001_trail_micro-service/models.py
+++ comp2001_trail_micro-service/models.py
@@ -28,6 +28,18 @@ class Location(db.Model):
     Longitude = db.Column(db.Float, nullable=False)
     Sequence = db.Column(db.Integer, nullable=False)
 
+class Tag(db.Model):
+    __tablename__ = "Tags"
+    __table_args__ = {"schema": "TrailApp"}
+    TagID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Name = db.Column(db.String(255), nullable=False)
+
+class TrailTag(db.Model):
+    __tablename__ = "TrailTags"
+    __table_args__ = {"schema": "TrailApp"}
+    TrailID = db.Column(db.Integer, db.ForeignKey("TrailApp.trails.TrailID"), primary_key=True)
+    TagID = db.Column(db.Integer, db.ForeignKey("TrailApp.Tags.TagID"), primary_key=True)
+
 class TrailSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Trail
@@ -43,10 +55,17 @@ class LocationSchema(ma.SQLAlchemyAutoSchema):
         model = Location
         load_instance = True
 
+class TagSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tag
+        load_instance = True
+
 trail_schema = TrailSchema()
 trails_schema = TrailSchema(many=True)
 trail_info_schema = TrailInfoSchema()
 trail_infos_schema = TrailInfoSchema(many=True)
 location_schema = LocationSchema()
 locations_schema = LocationSchema(many=True)
+tag_schema = TagSchema()
+tags_schema = TagSchema(many=True)
 
