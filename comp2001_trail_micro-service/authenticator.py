8d9c7c594588b14aba3eadbc5da7b89b5c93c66e
Ness <146290658+IronNess@users.noreply.github.com>
2025-01-13 00:55:18 +0000

    Update authenticator.py
    Cleaned up the code slightly. Corrected typo in authenticator

--- comp2001_trail_micro-service/authenticator.py
+++ comp2001_trail_micro-service/authenticator.py
@@ -1,19 +1,18 @@
 import requests
 
-AUTHEENTICATOR_API_URL = "https://web.socem.plymouth.ac.uk/COMP2001/auth/api/users"
+AUTHENTICATOR_API_URL = "https://web.socem.plymouth.ac.uk/COMP2001/auth/api/users"
 
 def authenticate_user(email, password):
+    """Authenticate a user using the external Authenticator API."""
     try:
         response = requests.post(
-            f"{AUTHEENTICATOR_API_URL}/login",
+            f"{AUTHENTICATOR_API_URL}/login",
             json={"email": email, "password": password},
         )
         if response.status_code == 200:
-            user_data = response.json()
-            return user_data
+            return response.json()
         else:
             return None
     except Exception as e:
         print(f"Error connecting to Authenticator API: {e}")
         return None
-       
